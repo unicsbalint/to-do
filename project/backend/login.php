758a2053fae4190e6946201d4d605bb0e9080b9c
PHorvath1 <hpeti291@gmail.com>
2020-10-02 17:24:20 +0200

    Update login and register, added pictures and frontend folders

--- project/backend/login.php
+++ project/backend/login.php
@@ -3,4 +3,5 @@
 <input type="text" class="loginUser" placeholder="Username">
 <input type="password" class="loginUser" placeholder="Password">
 
+<button type="submit" name="login">Login</button>
 </form>
--- project/backend/register.php
+++ project/backend/register.php
@@ -1,8 +1,35 @@
-<form action="POST">
+<?php
 
-<input type="text" class="registerUser" placeholder="Username">
-<input type="password" class="registerUser" placeholder="Password">
-<input type="password" class="registerUser" placeholder="Confirm password">
-<input type="email" class="registerUser" placeholder="Email">
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
+        $postData = [
+            'uname' => $_POST['username'],
+            'email' => $_POST['email'],
+            'password' => $_POST['password'],
+            'confirmpassword' => $_POST['confirmpassword']
+        ];
+    }
 
+
+?>
+
+<form action="post">
+    <div>
+        <input type="text" class="registerUser" name="username" placeholder="Username">
+    </div>
+
+    <div>
+        <input type="email" class="registerUser" name="email" placeholder="Email">
+    </div>
+
+    <div>
+        <input type="password" class="registerUser" name="password" placeholder="Password">
+    </div>
+
+    <div>
+        <input type="password" class="registerUser" name="confirmpassword" placeholder="Confirm password">
+    </div>
+
+    <div>
+        <button type="submit" name="register">Register</button>
+    </div>
 </form>
