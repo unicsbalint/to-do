3ea8b93fa327a7eeaf867e8f8dff554c89e9727f
PHorvath1 <hpeti291@gmail.com>
2020-10-02 16:19:47 +0200

    Added folders for backend and pictures, user management files to project

---
+++ project/backend/login.php
@@ -0,0 +1,6 @@
+<form action="POST">
+
+<input type="text" class="loginUser" placeholder="Username">
+<input type="password" class="loginUser" placeholder="Password">
+
+</form>
---
+++ project/backend/register.php
@@ -0,0 +1,8 @@
+<form action="POST">
+
+<input type="text" class="registerUser" placeholder="Username">
+<input type="password" class="registerUser" placeholder="Password">
+<input type="password" class="registerUser" placeholder="Confirm password">
+<input type="email" class="registerUser" placeholder="Email">
+
+</form>
