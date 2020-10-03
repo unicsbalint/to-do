90ff24e338b94efda015c5da22fb377f5a556314
HorKrisz <horvathkrisz00@gmail.com>
2020-10-03 16:56:13 +0200

    Added database connection functions

---
+++ project/backend/dbFunctions.php
@@ -0,0 +1,27 @@
+<?
+
+function databaseConnect() {
+    $connection = new PDO('mysql:host=localhost;dbname=todo;','root','');
+    $connection->exec("SET NAMES 'utf8'");
+    return $connection;
+}
+
+function getRecord($queryString, $queryParams = []) {
+    $connection = databaseConnect();
+    $statement = $connection->prepare($queryString);
+    $success = $statement->execute($queryParams);
+    $result = $success ? $statement->fetch(PDO::FETCH_ASSOC) : [];
+    $statement->closeCursor();
+    $connection = null;
+    return $result;
+}
+
+function executeDML($queryString, $queryParams = []) {
+    $connection = databaseConnect();
+    $statement = $connection->prepare($queryString);
+    $success = $statement->execute($queryParams);
+    $statement->closeCursor();
+    $connection = null;
+    return $success;
+}
+?>
