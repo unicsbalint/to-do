9d9ca1158066aa21f8f9b9294842ce1942cb80d0
Erdélyi Roland <erdelyi.roland99@icloud.com>
2020-10-05 02:06:42 +0200

    Added new navigation system

---
+++ project/backend/nav.php
@@ -0,0 +1,24 @@
+<hr>
+
+<ul class="nav nav-pills nav-fill">
+        <li class="nav-item">
+            <a class="nav-link <?=$_GET['P'] == 'home' ? 'active' : ''?>" data-toggle="tab" href="index.php?P=home">Home</a>
+        </li>
+        <?php if(!IsUserLoggedIn()) : ?>
+            <li class="nav-item">
+                <a class="nav-link <?=$_GET['P'] == 'login' ? 'active' : ''?>" data-toggle="tab" href="index.php?P=login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?=$_GET['P'] == 'registration' ? 'active' : ''?>" data-toggle="tab" href="index.php?P=registration">Registration</a>
+            </li>
+        <?php else : ?>
+            <li class="nav-item">
+            <a class="nav-link <?=$_GET['P'] == 'todo' ? 'active' : ''?>" data-toggle="tab" href="index.php?P=todo">TO-DO List</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"href="index.php?P=logout">Logout</a>
+            </li>
+        <?php endif; ?>
+    </ul>
+
+<hr>
