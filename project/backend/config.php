4cbb75fd8063de1c1016832531e6b49ef1827b51
Erdélyi Roland <erdelyi.roland99@icloud.com>
2020-10-05 02:06:00 +0200

    Added config.php

---
+++ project/backend/config.php
@@ -0,0 +1,8 @@
+<?php 
+    define('BASE_DIR', './project/');
+    define('FRONTEND_DIR', BASE_DIR.'frontend/');
+    define('BACKEND_DIR', BASE_DIR.'backend/');
+
+    define('DATABASE_CONTROLLER', BACKEND_DIR.'dbFunctions.php');
+    define('USER_MANAGER', BACKEND_DIR.'userManager.php');
+?>
