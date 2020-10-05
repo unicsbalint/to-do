129acd498c2ba29ca53e1e6ae5e4c9cbc0aff9f4
Erdélyi Roland <erdelyi.roland99@icloud.com>
2020-10-05 02:07:45 +0200

    Added routing.php for the new navigation system

---
+++ project/backend/routing.php
@@ -0,0 +1,33 @@
+<?php
+    if(!array_key_exists('P', $_GET) || empty($_GET['P']))
+	    $_GET['P'] = 'home';
+
+    
+    switch ($_GET['P']) 
+    {
+        case 'home': 
+            require_once BACKEND_DIR.'home.php'; 
+            break;
+        
+
+        case 'login':
+            !IsUserLoggedIn() ? require_once BACKEND_DIR.'user/login.php' : header('Location: index.php?P=home');
+            break;
+            
+        case 'registration':
+            !IsUserLoggedIn() ? require_once BACKEND_DIR.'user/register.php' : header('Location: index.php?P=home');
+            break;
+
+        case 'todo':  
+            IsUserLoggedIn() ? require_once BACKEND_DIR.'todo.php' : header('Location: index.php?P=home');
+            break;
+
+        case 'logout':  
+            IsUserLoggedIn() ? UserLogout() : header('Location: index.php?P=home');
+            break;
+
+        default:
+            require_once BACKEND_DIR.'404.php'; 
+            break;
+    }
+?>
